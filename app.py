9f6e24f4f98e0ede3d9d2e1c0032c01a3c7b8147
stundrg <khr0106@naver.com>
2025-03-24 09:40:46 +0900

    푸시

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("2024 KOBIS Daily Box Office 데이터 분석")
 @st.cache_data 
 def load_data():
     # path = "/home/tom/data/movie_after/dailyboxoffice"
-    path = "data/movie_after_2024.parquet"
+    path = "/home/wsl/data/movie_after/dailyboxoffice"
     dataset = ds.dataset(path, format="parquet", partitioning="hive")
     df = dataset.to_table().to_pandas()
      # dt를 datetime으로 변환 후 YYYY-MM-DD 문자열로 포맷
